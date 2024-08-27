eaaffebc1edbf396c197eb316502e6c97b4c5f43
rootcrop <rootcrop@gmail.com>
2024-08-27 21:24:17 +0300

    Add files via upload

---
+++ modules9_3functionals.py
@@ -0,0 +1,18 @@
+
+''' Задача: Дано 2 списка:
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+Необходимо создать 2 генераторных сборки:
+1 В переменную first_result запишите генераторную сборку, которая высчитывает 
+    разницу длин строк из списков first и second, если их длины не равны. Для 
+    перебора строк попарно из двух списков используйте функцию zip.
+2 В переменную second_result запишите генераторную сборку, которая содержит 
+    результаты сравнения длин строк в одинаковых позициях из списков first и second. 
+    Составьте эту сборку НЕ используя функцию zip. Используйте функции range и len. '''
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+first_result = (len(x)-len(y) for x, y in zip(first, second) if len(x)!=len(y))   # генератор в скобках пишется
+second_result = (len(first[x])==len(second[x]) for x in range(len(first)))
+print(list(first_result))
+print(list(second_result))
