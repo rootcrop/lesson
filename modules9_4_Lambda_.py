ea11fe39688246c74446b7c4bdf567ee421773b2
rootcrop <rootcrop@gmail.com>
2024-08-29 18:56:34 +0300

    Update modules9_4_Lambda_.py

--- modules9_4_Lambda_.py
+++ modules9_4_Lambda_.py
@@ -1,27 +1,26 @@
+from random import choice
+
 ''' Задача "Функциональное разнообразие":
-Lambda-функция:
-Даны 2 строки:
+Lambda-функция: Даны 2 строки:
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 Необходимо составить lambda-функцию для следующего выражения - list(map(?, first, second)).
 Здесь ? - место написания lambda-функции.
 Результатом должен быть список совпадения букв в той же позиции:
 [False, True, True, False, False, False, False, False, True, False, False, False, False, False]
-Где True - совпало, False - не совпало. 
-
-Замыкание: 
-Напишите функцию get_advanced_writer(file_name), принимающую название файла для записи.
-Внутри этой функции, напишите ещё одну - write_everything(*data_set), где *data_set - параметр принимающий неограниченное количество данных любого типа.
-Логика write_everything заключается в добавлении в файл file_name всех данных из data_set в том же виде.
-Функция get_advanced_writer возвращает функцию write_everything.
-Данный код:
-write = get_advanced_writer('example.txt')
-write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке']) '''
+Где True - совпало, False - не совпало.'''
 
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 print( res:= list(map(lambda x,y: x==y, first, second)) )
 
+''' Замыкание: Напишите функцию get_advanced_writer(file_name), принимающую название файла для записи.
+Внутри этой функции, напишите ещё одну - write_everything(*data_set), где *data_set - параметр 
+принимающий неограниченное количество данных любого типа.
+Логика write_everything заключается в добавлении в файл file_name всех данных из data_set в том же виде.
+Функция get_advanced_writer возвращает функцию write_everything.
+Данный код: write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке']) '''
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
         with open(file_name, 'a', encoding='utf-8') as f:
@@ -31,3 +30,17 @@ def get_advanced_writer(file_name):
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
 
+
+''' Метод __call__:
+Создайте класс MysticBall, объекты которого обладают атрибутом words хранящий коллекцию строк.
+В этом классе также определите метод __call__ который будет случайным образом выбирать слово из words 
+и возвращать его. Для случайного выбора с одинаковой вероятностью для каждого данного в коллекции можете 
+использовать функцию choice из модуля random. '''
+class MysticBall:
+    def __init__(self, *words):
+        self.words=words
+    def __call__(self):     # если есть такой метод у класса, то его объект можно вызвать как функцию
+        return choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball()); print(first_ball()); print(first_ball())
